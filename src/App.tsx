54d302ad92ef28428d254da4814ecce216026538
Raphael Machado <raphael.mm.91@gmail.com>
2023-05-08 23:46:35 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { ChangeEvent, useState } from "react";
 import { marked } from "marked";
 function App() {
-  const [markdown, setMarkdown] = useState("");
+  const [markdown, setMarkdown] = useState(
+    "# Markdown Previewer \n\n## FreeCodeCamp challenge\n `<div>html<div>` \n ```const add = (a = 1,b = 1)=> a + b``` \n ***bold*** _itallic_ **_both_  \n [freecodecamp](https://www.freecodecamp.org) ~~crossing stuff out~~  \n > Block Quotes! Wild Header | Crazy Header | Another Header? \n - And of course there are lists.\n - Some are bulleted. \n  - With different indentation levels.  - That look like this. \n ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)"
+  );
 
   function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
     setMarkdown(event.target.value);
@@ -11,12 +13,12 @@ function App() {
       {" "}
       <div className="input">
         <header>Editor</header>
-        <textarea value={markdown} onChange={handleChange} />
+        <textarea id="editor" value={markdown} onChange={handleChange} />
       </div>
       <div className="output">
         <header>Previewer</header>
         <div
-          id="previewer"
+          id="preview"
           dangerouslySetInnerHTML={{ __html: marked(markdown) }}
         />
       </div>
